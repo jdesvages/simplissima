fdd8e93e799b43f5e55c35eded622e124477b570
ylebriand <ylebriand@newcom-regie.com>
2019-02-15 17:17:50 +0100

    init repo

---
+++ src/affiliates-jekyll/_site/assets/js/app.js
@@ -0,0 +1,11 @@
+// Initialise FlexSlider for Carousels
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "fade",
+        // controlNav: "thumbnails",
+        directionNav: true,
+        slideshowSpeed: 5000,
+        animationSpeed: 600,
+        touch: true
+    });
+});
